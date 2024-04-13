d0c16ba3993d9950c32a7bd10d20b3f488d08569
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2024-04-13 11:42:12 -0700

    update

--- scripts/tag-n-release.sh
+++ scripts/tag-n-release.sh
@@ -1,58 +1,50 @@
 #!/usr/bin/env bash
 
-# Define the path to the modules directory
-MODULES_DIR="modules"
+# Define the URL of the version.yaml file in the main branch
+MAIN_BRANCH_VERSION_FILE="https://raw.githubusercontent.com/pgaijin66/terraform-versioned/main/modules/rds/version.yaml"
 
-declare current_version
-declare main_version
+# Define the path to the version.yaml file in the current branch
+CURRENT_BRANCH_VERSION_FILE="modules/rds/version.yaml"
 
-function get_current_version () {
-    version_file=$1
-    current_version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"') 
+# Function to fetch content from URL
+fetch_version_file_content() {
+    curl -s "$1"
 }
 
-function get_main_version () {
-    version_file=$1
+# Fetch version.yaml content from the main branch
+main_branch_content=$(fetch_version_file_content "$MAIN_BRANCH_VERSION_FILE")
 
-    # # Fetch the latest changes from the master branch
-    # git fetch origin main
+# Fetch version.yaml content from the current branch
+current_branch_content=$(<"$CURRENT_BRANCH_VERSION_FILE")
 
-    # Check if the fetch was successful
-    if [[ ! "$(git fetch origin main)" ]]; then
-        echo "Error: Failed to fetch changes from master branch."
-        exit 1
-    fi
+# Extract versions
+main_version=$(echo "$main_branch_content" | awk '/^version:/ {print $2}' | tr -d '"')
+current_version=$(echo "$current_branch_content" | awk '/^version:/ {print $2}' | tr -d '"')
 
-    # Checkout the master branch
-    git checkout main
+# Get folder name
+folder_name=$(dirname "$CURRENT_BRANCH_VERSION_FILE")
+DIRECTORY="modules"
+folders=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -type d)
+FOLDER_TO_KEEP="$folder_name"
 
-    main_version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"') 
-
-    # Return to the original branch
-    git checkout -
-
-}
-
-# Iterate over each folder inside the modules directory
-for folder in "$MODULES_DIR"/*/; do
-    folder=${folder%/}
-    version_file="$folder/version.yaml"
-
-    # Check if the version.yaml file exists in the current folder
-    if [ -f "$version_file" ]; then
-        # Extract the version using awk
-        # version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"')
-        get_current_version "$version_file"
-
-        get_main_version "$version_file"
-
-        echo "$current_version"
-        echo "$main_version"
-
-
-    #     # Print the version along with the folder name
-    #     echo "Version in $folder: $current_version"
-    # else
-    #     echo "Error: version.yaml not found in $folder"
-    fi
-done
+# Compare versions
+if [ "$main_version" != "$current_version" ]; then
+    echo "Version has changed in folder $folder_name from $main_version to $current_version"
+    # tag_name="$current_version"
+    # git tag "$tag_name" && git push origin "$tag_name"
+    for folder in $folders; do
+        folder_name=$(basename "$folder") 
+        folder_to_keep=$(basename "$FOLDER_TO_KEEP")
+        if [ "$folder_name" != "$folder_to_keep" ]; then
+            tag_name="$current_version"
+            git checkout -b intermediate-branch
+            git add .
+            rm -rf "$folder"
+            git commit -m "Publishing module: $folder_name"
+            git tag "$tag_name" && git push origin "$tag_name"
+            git checkout -
+        fi
+    done
+else
+    echo "Version is unchanged in folder $folder_name ($main_version)"
+fi
