8fb838bc1b177753d20f5d3283bae03f962b57df
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2024-04-13 13:37:23 -0700

    update

--- scripts/tag-n-release.sh
+++ scripts/tag-n-release.sh
@@ -24,7 +24,6 @@
 # # Get folder name
 # folder_name=$(dirname "$CURRENT_BRANCH_VERSION_FILE")
 # DIRECTORY="modules"
-# folders=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -type d)
 # FOLDER_TO_KEEP="$folder_name"
 
 # # Compare versions
@@ -54,6 +53,7 @@ START_DIR="."
 
 # Find all version.yaml files
 version_files=$(find "$START_DIR" -type f -name "version.yaml")
+folders=$(find "$START_DIR" -mindepth 1 -maxdepth 3 -type d)
 
 # # Define the URL of the version.yaml file in the main branch
 
@@ -78,21 +78,56 @@ function get_main_branch_version(){
 # main_branch_content=$(fetch_version_file_content "$MAIN_BRANCH_VERSION_FILE")
 
 # Print the paths of all version.yaml files
+git checkout -b intermediate-branch
+
 for file in $version_files; do
     file="${file#./}"
+    echo $file
 
     rds=$(awk '/^rds:/ {print $2}' "$file" | tr -d '"')
     provisioned=$(awk '/^provisioned:/ {print $2}' "$file" | tr -d '"')
 
 
     fp=$(dirname "$file")
-    fp="${fp#./}"
 
     current_branch_content=$(<"$file")
 
     current_version=$(echo "$current_branch_content" | awk '/^version:/ {print $2}' | tr -d '"')
-    main_branch_version=$(get_main_branch_version "$file")
+    main_branch_version=$(get_main_branch_version "$file" | awk '/^version:/ {print $2}' | tr -d '"')
 
     echo $current_version
     echo $main_branch_version
+
+    folder_path=$(dirname "$(dirname "$(dirname "$file")")")
+
+
+    if [ "$main_branch_version" != "$current_version" ]; then
+        # git checkout -b intermediate-branch
+        # tag_name="$current_version"
+        # git tag "$tag_name" && git push origin "$tag_name"
+        FOLDER_TO_KEEP="./$fp"
+        for folder in $folders; do
+            folder_name=$(basename "$folder") 
+            echo $folder
+            folder_to_keep=$(basename "$FOLDER_TO_KEEP")
+            if [ "$folder_name" != "$folder_to_keep" ]; then
+                tag_name="$current_version"
+                # rm -rf "$folder"
+                echo "will delete  $folder"
+            fi
+        done
+        echo "./$fp"
+        echo "Folder path: $folder_path"
+
+        echo "will delete  ./$fp"
+
+
+
+    fi
 done
+
+git add .
+git commit -m "Publishing module: $folder_name from $main_version to $current_version"
+git tag "$tag_name" && git push origin "$tag_name"
+git checkout -
+git branch -d intermediate-branch
