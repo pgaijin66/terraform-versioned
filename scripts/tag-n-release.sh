1405a54a60c91f04c708030b8a375ce864c8ac1e
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2024-04-13 10:48:01 -0700

    update

--- scripts/tag-n-release.sh
+++ scripts/tag-n-release.sh
@@ -3,6 +3,36 @@
 # Define the path to the modules directory
 MODULES_DIR="modules"
 
+declare current_version
+declare main_version
+
+function get_current_version () {
+    version_file=$1
+    current_version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"') 
+}
+
+function get_main_version () {
+    version_file=$1
+
+    # # Fetch the latest changes from the master branch
+    # git fetch origin main
+
+    # Check if the fetch was successful
+    if [[ ! "$(git fetch origin main)" ]]; then
+        echo "Error: Failed to fetch changes from master branch."
+        exit 1
+    fi
+
+    # Checkout the master branch
+    git checkout main
+
+    main_version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"') 
+
+    # Return to the original branch
+    git checkout -
+
+}
+
 # Iterate over each folder inside the modules directory
 for folder in "$MODULES_DIR"/*/; do
     folder=${folder%/}
@@ -11,11 +41,18 @@ for folder in "$MODULES_DIR"/*/; do
     # Check if the version.yaml file exists in the current folder
     if [ -f "$version_file" ]; then
         # Extract the version using awk
-        version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"')
+        # version=$(awk '/^version:/ {print $2}' "$version_file" | tr -d '"')
+        get_current_version "$version_file"
 
-        # Print the version along with the folder name
-        echo "Version in $folder: $version"
-    else
-        echo "Error: version.yaml not found in $folder"
+        get_main_version "$version_file"
+
+        echo "$current_version"
+        echo "$main_version"
+
+
+    #     # Print the version along with the folder name
+    #     echo "Version in $folder: $current_version"
+    # else
+    #     echo "Error: version.yaml not found in $folder"
     fi
 done
